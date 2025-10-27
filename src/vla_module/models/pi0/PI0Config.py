17e86ff485859620da03c27525ae8e4512cee104
Shiven Saini <shiven.career@proton.me>
2025-10-27 12:07:44 +0530

    Feat(pi0): implemented pi0 config, withExpert
    Core challenge remains on how to decouple the stuff similarly

---
+++ src/vla_module/models/pi0/PI0Config.py
@@ -0,0 +1,97 @@
+# Author : Shiven Saini
+# Email : shiven.career@proton.me
+
+"""
+PI0Config Data
+This file act as a Single source of truth for any config, hyperparameters etc PI0Policy may require.
+"""
+
+import torch
+from torch import nn
+from transformers.models.auto import CONFIG_MAPPING
+from transformers.models.gemma import modeling_gemma
+from transformers import (
+    PaliGemmaForConditionalGeneration,
+    GemmaForCausalLM
+)
+
+
+
+
+# GemmaConfig taken from OpenPI Repository
+class GemmaConfig:
+    """Configuration for Gemma model variants."""
+
+    def __init__(self, width, depth, mlp_dim, num_heads, num_kv_heads, head_dim):
+        self.width = width
+        self.depth = depth
+        self.mlp_dim = mlp_dim
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.head_dim = head_dim
+
+
+gemma_language = GemmaConfig(
+            width=2048,
+            depth=18,
+            mlp_dim=16_384,
+            num_heads=8,
+            num_kv_heads=1,
+            head_dim=256,
+        )
+
+gemma_expert = GemmaConfig(
+            width=1024,
+            depth=18,
+            mlp_dim=4096,
+            num_heads=8,
+            num_kv_heads=1,
+            head_dim=256,
+        )
+
+# TODO: SHIVEN -> Need to take care of this, added just to see how much similarity I can decouple from each VLA Policy
+
+# VLM Language part
+_text_config = CONFIG_MAPPING["gemma"](
+    hidden_size = gemma_language.width,
+    intermediate_size = gemma_language.mlp_dim,
+    num_attention_heads = gemma_language.num_heads,
+    head_dim = gemma_language.head_dim,
+    num_hidden_layers = gemma_language.depth,
+    num_key_value_heads = gemma_language.num_kv_heads,
+    hidden_activation = "gelu_pytorch_tanh",
+    torch_dtype = "float32",
+    vocab_size = 257152,
+    use_adarms = use_adarms[0],
+    adarms_cond_dim = gemma_language.width if use_adarms[0] else None,
+)
+
+# VLM vision tower aka siglip vision encoder
+_vision_config = CONFIG_MAPPING["siglip_vision_model"](
+    intermediate_size = 4304,
+    projection_dim = 2048,
+    projector_hidden_act = "gelu_fast",
+    torch_dtype = "float32"
+)
+
+vlm_config = CONFIG_MAPPING["paligemma"](
+    _vocab_size = 257152,  # noqa: SLF001
+    image_token_index = 257152,
+    text_config = _text_config_hf,
+    vision_config = _vision_config_hf,
+)
+
+
+action_expert_config = CONFIG_MAPPING["gemma"](
+    head_dim=gemma_expert.head_dim,
+    hidden_size=gemma_expert.width,
+    intermediate_size=gemma_expert.mlp_dim,
+    num_attention_heads=gemma_expert.num_heads,
+    num_hidden_layers=gemma_expert.depth,
+    num_key_value_heads=gemma_expert.num_kv_heads,
+    vocab_size=257152,
+    hidden_activation="gelu_pytorch_tanh",
+    torch_dtype="float32",
+    use_adarms=use_adarms[1],
+    adarms_cond_dim=gemma_expert.width if use_adarms[1] else None,
+)
---
+++ src/vla_module/models/pi0/PI0Policy.py
@@ -0,0 +1,241 @@
+import torch
+from torch import nn
+from PaliGemmaWithActionExpert import PaliGemmaWithActionExpert
+from .PI0Config import vlm_config, action_expert_config
+
+class PI0Pytorch(nn.Module):
+    """Core PI0 PyTorch model - Inference Only."""
+
+    def __init__(self, config: PI0Config):
+        super().__init__()
+        self.config = config
+
+
+        self.paligemma_with_expert = PaliGemmaWithActionExpert(
+            vlm_config,
+            action_expert_config,
+            use_adarms=[False, False],
+            precision=config.dtype,
+        )
+
+        self.action_in_proj = nn.Linear(config.max_action_dim, action_expert_config.width)
+        self.action_out_proj = nn.Linear(action_expert_config.width, config.max_action_dim)
+
+        self.state_proj = nn.Linear(config.max_state_dim, action_expert_config.width)
+        self.action_time_mlp_in = nn.Linear(2 * action_expert_config.width, action_expert_config.width)
+        self.action_time_mlp_out = nn.Linear(action_expert_config.width, action_expert_config.width)
+
+        # Compile model if requested
+        if config.compile_model:
+            torch.set_float32_matmul_precision("high")
+            self.sample_actions = torch.compile(self.sample_actions, mode=config.compile_mode)
+
+        msg = """An incorrect transformer version is used, please create an issue on https://github.com/huggingface/lerobot/issues"""
+
+        try:
+            from transformers.models.siglip import check
+
+            if not check.check_whether_transformers_replace_is_installed_correctly():
+                raise ValueError(msg)
+        except ImportError:
+            raise ValueError(msg) from None
+
+    def _prepare_attention_masks_4d(self, att_2d_masks):
+        """Helper method to prepare 4D attention masks for transformer."""
+        att_2d_masks_4d = att_2d_masks[:, None, :, :]
+        return torch.where(att_2d_masks_4d, 0.0, OPENPI_ATTENTION_MASK_VALUE)
+
+    def sample_noise(self, shape, device):
+        """Sample Gaussian noise for denoising process."""
+        return torch.normal(
+            mean=0.0,
+            std=1.0,
+            size=shape,
+            dtype=torch.float32,
+            device=device,
+        )
+
+    def embed_prefix(
+            self, images, img_masks, lang_tokens, lang_masks
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Embed images with SigLIP and language tokens with embedding layer."""
+        embs = []
+        pad_masks = []
+        att_masks = []
+
+        # Process images
+        for img, img_mask in zip(images, img_masks, strict=True):
+            img_emb = self.paligemma_with_expert.embed_image(img)
+            bsize, num_img_embs = img_emb.shape[:2]
+
+            embs.append(img_emb)
+            pad_masks.append(img_mask[:, None].expand(bsize, num_img_embs))
+            att_masks += [0] * num_img_embs
+
+        # Process language tokens
+        lang_emb = self.paligemma_with_expert.embed_language_tokens(lang_tokens)
+        lang_emb_dim = lang_emb.shape[-1]
+        lang_emb = lang_emb * math.sqrt(lang_emb_dim)
+
+        embs.append(lang_emb)
+        pad_masks.append(lang_masks)
+
+        num_lang_embs = lang_emb.shape[1]
+        att_masks += [0] * num_lang_embs
+
+        embs = torch.cat(embs, dim=1)
+        pad_masks = torch.cat(pad_masks, dim=1)
+        att_masks = torch.tensor(att_masks, dtype=torch.bool, device=pad_masks.device)
+
+        bsize = pad_masks.shape[0]
+        att_masks = att_masks[None, :].expand(bsize, len(att_masks))
+
+        return embs, pad_masks, att_masks
+
+    def embed_suffix(self, state, noisy_actions, timestep):
+        """Embed state, noisy_actions, timestep to prepare for Expert Gemma processing."""
+        embs = []
+        pad_masks = []
+        att_masks = []
+
+        if self.state_proj.weight.dtype == torch.float32:
+            state = state.to(torch.float32)
+
+        state_emb = self.state_proj(state)
+        embs.append(state_emb[:, None, :])
+        bsize = state_emb.shape[0]
+        device = state_emb.device
+
+        state_mask = torch.ones(bsize, 1, dtype=torch.bool, device=device)
+        pad_masks.append(state_mask)
+        att_masks += [1]
+
+        # Embed timestep using sine-cosine positional encoding
+        time_emb = create_sinusoidal_pos_embedding(
+            timestep,
+            self.action_in_proj.out_features,
+            min_period=self.config.min_period,
+            max_period=self.config.max_period,
+            device=timestep.device,
+        )
+        time_emb = time_emb.type(dtype=timestep.dtype)
+
+        # Fuse timestep + action information using an MLP
+        action_emb = self.action_in_proj(noisy_actions)
+        time_emb = time_emb[:, None, :].expand_as(action_emb)
+        action_time_emb = torch.cat([action_emb, time_emb], dim=2)
+
+        x = self.action_time_mlp_in(action_time_emb)
+        x = F.silu(x)
+        action_time_emb = self.action_time_mlp_out(x)
+        adarms_cond = None
+
+        embs.append(action_time_emb)
+        bsize, action_time_dim = action_time_emb.shape[:2]
+        action_time_mask = torch.ones(bsize, action_time_dim, dtype=torch.bool, device=timestep.device)
+        pad_masks.append(action_time_mask)
+
+        # Set attention masks so that image, language and state inputs do not attend to action tokens
+        att_masks += [1] + ([0] * (self.config.chunk_size - 1))
+
+        embs = torch.cat(embs, dim=1)
+        pad_masks = torch.cat(pad_masks, dim=1)
+        att_masks = torch.tensor(att_masks, dtype=embs.dtype, device=embs.device)
+        att_masks = att_masks[None, :].expand(bsize, len(att_masks))
+
+        return embs, pad_masks, att_masks, adarms_cond
+
+    @torch.no_grad()
+    def sample_actions(
+            self, images, img_masks, lang_tokens, lang_masks, state, noise=None, num_steps=None
+    ) -> Tensor:
+        """Do a full inference forward and compute the action."""
+        if num_steps is None:
+            num_steps = self.config.num_inference_steps
+
+        bsize = state.shape[0]
+        device = state.device
+
+        if noise is None:
+            # Sample noise with padded dimension as expected by action_in_proj
+            actions_shape = (
+                bsize,
+                self.config.chunk_size,
+                self.config.max_action_dim,
+            )
+            noise = self.sample_noise(actions_shape, device)
+
+        prefix_embs, prefix_pad_masks, prefix_att_masks = self.embed_prefix(
+            images, img_masks, lang_tokens, lang_masks
+        )
+        prefix_att_2d_masks = make_att_2d_masks(prefix_pad_masks, prefix_att_masks)
+        prefix_position_ids = torch.cumsum(prefix_pad_masks, dim=1) - 1
+
+        prefix_att_2d_masks_4d = self._prepare_attention_masks_4d(prefix_att_2d_masks)
+        self.paligemma_with_expert.paligemma.language_model.config._attn_implementation = "eager"
+
+        _, past_key_values = self.paligemma_with_expert.forward(
+            attention_mask=prefix_att_2d_masks_4d,
+            position_ids=prefix_position_ids,
+            past_key_values=None,
+            inputs_embeds=[prefix_embs, None],
+            use_cache=True,
+        )
+
+        dt = -1.0 / num_steps
+        dt = torch.tensor(dt, dtype=torch.float32, device=device)
+
+        x_t = noise
+        time = torch.tensor(1.0, dtype=torch.float32, device=device)
+        while time >= -dt / 2:
+            expanded_time = time.expand(bsize)
+            v_t = self.denoise_step(
+                state,
+                prefix_pad_masks,
+                past_key_values,
+                x_t,
+                expanded_time,
+            )
+            x_t = x_t + dt * v_t
+            time += dt
+
+        return x_t
+
+    def denoise_step(
+            self,
+            state,
+            prefix_pad_masks,
+            past_key_values,
+            x_t,
+            timestep,
+    ):
+        """Apply one denoising step of the noise `x_t` at a given timestep."""
+        suffix_embs, suffix_pad_masks, suffix_att_masks, adarms_cond = self.embed_suffix(state, x_t, timestep)
+
+        suffix_len = suffix_pad_masks.shape[1]
+        batch_size = prefix_pad_masks.shape[0]
+        prefix_len = prefix_pad_masks.shape[1]
+
+        prefix_pad_2d_masks = prefix_pad_masks[:, None, :].expand(batch_size, suffix_len, prefix_len)
+        suffix_att_2d_masks = make_att_2d_masks(suffix_pad_masks, suffix_att_masks)
+        full_att_2d_masks = torch.cat([prefix_pad_2d_masks, suffix_att_2d_masks], dim=2)
+
+        prefix_offsets = torch.sum(prefix_pad_masks, dim=-1)[:, None]
+        position_ids = prefix_offsets + torch.cumsum(suffix_pad_masks, dim=1) - 1
+
+        full_att_2d_masks_4d = self._prepare_attention_masks_4d(full_att_2d_masks)
+        self.paligemma_with_expert.gemma_expert.model.config._attn_implementation = "eager"
+
+        outputs_embeds, _ = self.paligemma_with_expert.forward(
+            attention_mask=full_att_2d_masks_4d,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=[None, suffix_embs],
+            use_cache=False,
+            adarms_cond=[None, adarms_cond],
+        )
+
+        suffix_out = outputs_embeds[1]
+        suffix_out = suffix_out[:, -self.config.chunk_size:]
+        suffix_out = suffix_out.to(dtype=torch.float32)
+        return self.action_out_proj(suffix_out)
